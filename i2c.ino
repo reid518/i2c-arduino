794638499b02f034dabc8c1eb7811fca512ffdfb
Reid <reid.h.albrecht@wmich.edu>
2019-10-29 18:18:16 -0400

    All sensors working

--- i2c.ino
+++ i2c.ino
@@ -13,24 +13,22 @@
 #include <Wire.h>
 #include <NewPing.h>
 
-#define SONAR_NUM 1      // Number of sensors.
+#define SONAR_NUM 5      // Number of sensors.
 #define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
 
-const int ledPin = 13; // onboard LED
-static_assert(LOW == 0, "Expecting LOW to be 0");
 char c = 0;
-int num0 = 250;
-int num1 = 50;
-int num2 = 25;
-int num3 = 100;
-int num4 = 13;
-int sensor = 0;
+int sensor0 = 0;
+int sensor1 = 0;
+int sensor2 = 0;
+int sensor3 = 0;
+int sensor4 = 0;
 
 NewPing sonar[SONAR_NUM] = {   // Sensor object array.
   NewPing(13, 12, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping. 
-  //NewPing(9, 8, MAX_DISTANCE) 
-  //NewPing(7, 6, MAX_DISTANCE),
-  //NewPing(3, 2, MAX_DISTANCE)
+  NewPing(11, 10, MAX_DISTANCE), 
+  NewPing(9, 8, MAX_DISTANCE),
+  NewPing(7, 6, MAX_DISTANCE),
+  NewPing(5, 4, MAX_DISTANCE)
 };
 
 void setup() {
@@ -38,22 +36,35 @@ void setup() {
   Wire.begin(0x8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Wire.onRequest(sendData);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW); // turn it off
   Serial.println("Ready!");
 }
 
 void loop() {
   delay(100);
   for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
-    delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    delay(100); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
     Serial.print(i);
     Serial.print("=");
-    sensor = sonar[i].ping_cm();
-    Serial.print(sensor);
+    if (i == 0){
+      sensor0 = sonar[i].ping_cm();
+      Serial.print(sensor0);
+    } else if (i == 1){
+      sensor1 = sonar[i].ping_cm();
+      Serial.print(sensor1);
+    } else if (i == 2){
+      sensor2 = sonar[i].ping_cm();
+      Serial.print(sensor2);
+    } else if (i == 3){
+      sensor3 = sonar[i].ping_cm();
+      Serial.print(sensor3);
+    } else{
+      sensor4 = sonar[i].ping_cm();
+      Serial.print(sensor4);
+    }
     Serial.print("cm ");
+    Serial.println();
   }
-  Serial.println();
+  //Serial.println();
 
 }
 
@@ -63,26 +74,21 @@ void receiveEvent(int howMany) {
   while (Wire.available()) { // loop through all but the last
     c = Wire.read(); // receive byte as a character
     Serial.print("Data Received: ");
-    //digitalWrite(ledPin, c);
     Serial.println((uint8_t)c);
   }
 }
 
-
-
-
-
 void sendData(){
   if (c == 0){
-    Wire.write(sensor);
+//    Wire.write(sensor);
+    Wire.write(sensor0);
   } else if (c == 1){
-    Wire.write(num1);
+    Wire.write(sensor1);
   } else if (c == 2){
-    Wire.write(num2);
+    Wire.write(sensor2);
   } else if (c == 3){
-    Wire.write(num3);
+    Wire.write(sensor3);
   } else {
-    Wire.write(num4);
+    Wire.write(sensor4);
   }
-  //Wire.write("work");
 }
